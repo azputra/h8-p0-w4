ff8a7a746dc6957ab77b5e8dce7f459822a126a9
azputra <ahmadzp102@gmail.com>
2019-09-26 21:06:23 +0700

    belum

--- exercises-4.js
+++ exercises-4.js
@@ -1,11 +1,26 @@
 function cariModus(arr) {
-    // you can only write your code here!
-    const tes = []
+  // you can only write your code here!
+  for (var i = 0; i < arr.length; i++) {
+    for (var j = 0; j < arr.length; j++) {
+      if (arr[j] > arr[j + 1]) {
+        var temp = arr[j]
+        arr[j] = arr[j + 1]
+        arr[j + 1] = temp
+      }
+    }
   }
-  
-  // TEST CASES
-  console.log(cariModus([10, 4, 5, 2, 4])); // 4
-  console.log(cariModus([5, 10, 10, 6, 5])); // 5
-  console.log(cariModus([10, 3, 1, 2, 5])); // -1
-  console.log(cariModus([1, 2, 3, 3, 4, 5])); // 3
-  console.log(cariModus([7, 7, 7, 7, 7])); // -1
+  // console.log(arr)
+  var angka = [];
+  var jumlahAngka = [];
+  for (let i = 0; i < arr.length; i++) {
+    var tempAngka = angka.indexOf(arr[i])
+    console.log(tempAngka)
+  }
+}
+
+// TEST CASES
+console.log(cariModus([10, 4, 5, 2, 4])); // 4
+console.log(cariModus([5, 10, 10, 6, 5])); // 5
+console.log(cariModus([10, 3, 1, 2, 5])); // -1
+console.log(cariModus([1, 2, 3, 3, 4, 5])); // 3
+console.log(cariModus([7, 7, 7, 7, 7])); // -1
